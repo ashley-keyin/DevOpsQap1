ad1506554f2d0845a2295a27c40cb33f5c648149
ashley-keyin <ashley.mercer@keyin.com>
2022-04-01 00:18:15 -0230

    updated student and studentdemo

--- Student.java
+++ Student.java
@@ -21,18 +21,5 @@ public void displayRecord()
 {
     System.out.println("Student name is " + this.Name + " has age = " + this.Age);
 }
-
-public static void main(String[] args) {
-  Student S1 = new Student();
-  Student S2 = new Student();
-
-
-S1.setRecord("Ashley", 34);
-S2.setRecord("Ainee", 40);
-
-S1.displayRecord();
-S2.displayRecord();
-
-}
 }
 
---
+++ StudentDemo.java
@@ -0,0 +1,14 @@
+public class StudentDemo {
+  public static void main(String[] args) {
+    Student S1 = new Student();
+    Student S2 = new Student();
+  
+  
+  S1.setRecord("Ashley", 34);
+  S2.setRecord("Ainee", 40);
+  
+  S1.displayRecord();
+  S2.displayRecord();
+  
+  }
+}
