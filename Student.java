76545fecf7ef72c7a6ab09dc3131f2fc668f35ea
ashley-keyin <ashley.mercer@keyin.com>
2022-04-01 00:37:41 -0230

    add 2 new get methods and update displayRecord method

--- Student.java
+++ Student.java
@@ -5,21 +5,33 @@ public class Student{
 
 private String Name;
 private int Age;
+private String email;
+private String address;
 private static int count = 0;
 
   
 
    //behaviors
 
-public void setRecord(String n, int a)
+public void setRecord(String n, int a, String email, String address )
 {
   this.Name = n;
   this.Age = a;
+  this.email = email;
+  this.address = address;
+}
+
+public String getEmail() {
+  return email;
+}
+
+public String getAddress() {
+  return address;
 }
 
 public void displayRecord()
 {
-    System.out.println("Student name is " + this.Name + " has age = " + this.Age);
+    System.out.println("\n" + "Student name:" + this.Name + "\n" + "Age: " + this.Age + "\n" + "Email: " + this.email + "\n" + "Address: " + this.address);
 }
 }
 
